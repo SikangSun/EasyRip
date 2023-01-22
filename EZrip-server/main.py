961f2a4743fa8ea096a00a336a6abd7868128b2d
evanshaw <evan.shaw@gmail.com>
2023-01-22 00:04:12 -0500

    comined resolutions + metadata

--- EZrip-server/main.py
+++ EZrip-server/main.py
@@ -6,7 +6,6 @@ import json
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.middleware import Middleware
 
-
 # CORS STUFF
 origins = [
     "http://localhost:3000",
@@ -33,15 +32,20 @@ async def fake_video_streamer():
         yield b"some fake video bytes"
 
 
-def real_video_streamer(v):
-    proc = subprocess.Popen(["./yt-dlp", "-o", "-", "https://www.youtube.com/watch?v=" + v],
-                            stdout=subprocess.PIPE)
+def video_streamer(v, f):
+    if f == 0:
+        proc = subprocess.Popen(["./yt-dlp_macos", "-o", "-", "https://www.youtube.com/watch?v=" + v],
+                                stdout=subprocess.PIPE)
+    else:
+        proc = subprocess.Popen(["./yt-dlp_macos", "-o", "-", "-f", str(f), "https://www.youtube.com/watch?v=" + v],
+                                stdout=subprocess.PIPE)
+
     return proc.stdout
 
 
-@app.get("/")
-async def main(v):
-    return StreamingResponse(real_video_streamer(v), media_type="application/octet-stream")
+@app.get("/get_video")
+async def get_video(v, f: int = 0):
+    return StreamingResponse(video_streamer(v, f), media_type="application/octet-stream")
 
 
 @app.get("/get_metadata")
@@ -54,5 +58,37 @@ async def get_video_metadata(v):
         info = ydl.extract_info(url, download=False)
 
         # ℹ️ ydl.sanitize_info makes the info json-serializable
-        return json.dumps(ydl.sanitize_info(info))
+        metadata = ydl.sanitize_info(info)
+        print(metadata)
+        return_json = {'formats': [], 'title': metadata['title'], 'uploader': metadata['uploader'],
+                       'duration_string': metadata['duration_string'], 'like_count': metadata['like_count'],
+                       'upload_date': metadata['upload_date'], 'view_count': metadata['view_count']}
+        for f in metadata['formats']:
+            # skips videos without audio
+            # does not take into account videos without audio in the first place ???
+            # check if height is integer
+            if isinstance(f['height'], int):
+                height = f['height']
+            else:
+                height = 0
 
+            format_id = f['format_id']
+
+            # exclude storyboards
+            if f['format_note'] == "storyboard":
+                continue
+
+            if f['vcodec'] != "none" and f['acodec'] == "none":
+                if height <= 720:
+                    continue
+                else:
+                    # height greater than 720 but has no audio
+                    format_id = str(f['format_id']) + "+" + "bestaudio"
+
+            i = f['format'].index(' ')
+            desc = f['format'][f['format'].index(' ', i + 1) + 1:]
+            ext = f['ext']
+
+            return_json['formats'].append({'desc': desc, 'ext': ext, 'id': format_id})
+
+        return return_json
